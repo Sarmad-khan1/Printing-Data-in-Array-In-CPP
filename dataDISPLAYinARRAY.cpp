2b73c1f7dd36292cd34c54f08c8da79811a50557
Sarmad Khan <sarmadarbabkhan123@gmail.com>
2024-11-24 02:03:04 -0800

    Add files via upload

---
+++ dataDISPLAYinARRAY.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+	int element[5];
+	element[0]=1;
+	element[1]=2;
+	element[2]=3;
+	element[3]=4;
+	element[4]=5;
+
+	cout<<"Element in memory location 1 is: "<<element[1]<<endl;
+	cout<<"Element in memory location 3 is: "<<element[3]<<endl;
+}
